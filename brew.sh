61a12ad71e06ad6e1061a18cec7ea415cd9a7763
Marcus David Bronstein <marcusdb@gmail.com>
2021-12-08 02:16:12 +0000

    m1 pro changes

--- brew.sh
+++ brew.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # Install command-line tools using Homebrew
 
@@ -8,28 +7,14 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
+brew install stow
+
 brew install antigen
-# GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-brew install moreutils
-# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-brew install findutils
-# GNU `sed`, overwriting the built-in `sed`
-#brew install gnu-sed --with-default-names
 
-
-# z hopping around folders
-brew install z
+brew install zoxide
 
 #fast finder https://github.com/sharkdp/fd
 brew install fd
-
-# run this script when this file changes guy.
-#brew install entr
-
-# github util. gotta love `hub fork`, `hub create`, `hub checkout <PRurl>`
-#brew install hub
-
 
 # mtr - ping & traceroute. best.
 brew install mtr
@@ -43,27 +28,18 @@ brew install zsh-history-substring-search
 
 # Install other useful binaries
 brew install the_silver_searcher
-#brew install fzf
 
-brew install git
-#brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
-#brew install pv
-#brew install rename
-#brew install tree
-#brew install zopfli
-#brew install ffmpeg --with-libvpx
+brew install fzf
+
+brew install ffmpeg --with-libvpx
 
 brew install terminal-notifier
-
 
 brew install ncdu # find where your diskspace went
 
 brew install cdargs
 
 brew install tmux
-
-brew install fzf
 
 brew install thefuck
 # Remove outdated versions from the cellar
--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 ./brew.sh
 
-#cp .bash_profile ~/.bash_profile
-#cp .bash_prompt ~/.bash_prompt
-cp .zshrc ~/.zshrc
-cp .aliases ~/.aliases
-cp .functions ~/.functions
-cp .exports ~/.exports
-cp .inputrc ~/.inputrc
-cp .tmux.conf ~/.tmux.conf
+npm install -g statikk
+
+
+
+
+
+
 
