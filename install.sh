7b61c426ead6b5cafb6005f9fd2286867b822172
marcus bronstein <marcusdb@gmail.com>
2024-07-11 11:19:38 +0100

    new mac tuning

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
+xcodebuild -license
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#p10k installation GO TO HOME
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc.
+
+
+
 ./brew.sh
+
+#installs node 20
+nvm install 20 
 
 npm install -g statikk
 
